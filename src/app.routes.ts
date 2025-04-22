d0b5204fb9d86da0a7a2c6a1eef7c4871a944e07
Arthur Lucena <arthurviniciussl.contato@gmail.com>
2025-04-22 15:01:31 -0300

    New link for ms apps

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -2,7 +2,7 @@
 
 const AppRoutes = {
   home: '/',
-  warnings: "https://teams.microsoft.com/l/team/19%3A7k01k6Idv5z0G26Tkk5hE45SEW49py5XGGVd-VTXgNE1%40thread.tacv2/conversations?groupId=76730124-24c0-48d6-89bd-7386bcb5c644&tenantId=365e16bc-4066-4531-a922-56d85762ef8a",
+  warnings: "https://m365.cloud.microsoft/",
   msApps: "https://m365.cloud.microsoft/apps?auth=2&home=1",
   underConstruction: "/under-construction",
   team:"/team"
--- src/components/BugReport/index.tsx
+++ src/components/BugReport/index.tsx
@@ -9,14 +9,14 @@ export default function BugReport({ isOpen }: BugReportProps) {
 
     const art = {
         container: "art:w:fit art:p:base art:bg:yellow:10% art:hover:bg:yellow:20%  art:border-rd:lg art:border:thin art:hover:border:yellow-01 art:border:white-01 art:border:solid",
-        content: "art:font:yellow-01 art:font:semibold art:font:base art:flex art:row art:gap:base ",
+        content: "art:font:yellow-01 art:font:semibold art:font:base art:flex art:gap:base art:row",
     }
 
     return (
         <>
             <div className={art.container} style={{ "right": "0", "top": "0", "zIndex": "1" }} >
-                <a className={art.content} href="https://forms.office.com/r/sATFTy7hU6" target="_blank">
-                    <ToolTip msg="Reportar bug">
+                <ToolTip msg="Reportar bug">
+                    <a className={art.content} href="https://forms.office.com/r/sATFTy7hU6" target="_blank">
                         <BugIcon size={20} />
                         {
                             isOpen ?
@@ -30,8 +30,8 @@ export default function BugReport({ isOpen }: BugReportProps) {
                                     </>
                                 )
                         }
-                    </ToolTip >
-                </a>
+                    </a>
+                </ToolTip >
             </div >
         </>
     );
