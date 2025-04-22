7dff8e1c30a722644ce1fc432903ca62a6b3a869
Arthur Lucena <arthurviniciussl.contato@gmail.com>
2025-04-22 14:51:23 -0300

    New contact profiles

--- src/utils/Objects/TeamProfiles.ts
+++ src/utils/Objects/TeamProfiles.ts
@@ -1,23 +1,58 @@
 export const TeamProfiles = [
     {
-        "id":0,
-        "name":"arthur vinicius souza lucena",
-        "position":"estagiário", //software engineer
-        "email":"arthur.lucena@museumaccg.org.br",
-        "color_theme":"yellow"
+        "id": 0,
+        "name": "arthur vinicius souza lucena",
+        "position": "estagiário", //software engineer
+        "email": "arthur.lucena@museumaccg.org.br",
+        "color_theme": "yellow"
     },
     {
-        "id":1,
-        "name":"Suenia Karcia Freitas Paulino",
-        "position":"estagiário",
-        "email":"suenia.paulino@museumaccg.org.br",
-        "color_theme":"purple"
+        "id": 1,
+        "name": "Suenia Karcia Freitas Paulino",
+        "position": "estagiário",
+        "email": "suenia.paulino@museumaccg.org.br",
+        "color_theme": "purple"
     },
     {
-        "id":2,
-        "name":"Milena Soares Porto",
-        "position":"estagiário",
-        "email":"milena.porto@museumaccg.org.br",
-        "color_theme":"orange"
+        "id": 2,
+        "name": "Milena Soares Porto",
+        "position": "estagiário",
+        "email": "milena.porto@museumaccg.org.br",
+        "color_theme": "orange"
+    },
+    {
+        "id": 3,
+        "name": "Wiliane Pereira de Paiva",
+        "position": "estagiário",
+        "email": "wiliane.paiva@museumaccg.org.br",
+        "color_theme": "black"
+    },
+    {
+        "id": 4,
+        "name": "Karle Adrielle Alves da Silva",
+        "position": "Comercial",
+        "email": "karle.silva@museumaccg.org.br",
+        "color_theme": "green"
+    },
+    {
+        "id": 5,
+        "name": "Veronica Gomes dos Santos",
+        "position": "Recepção",
+        "email": "veronica.santos@museumaccg.org.br",
+        "color_theme": "blue"
+    },
+    {
+        "id": 6,
+        "name": "Ikaru Jeno Cassiano de Freitas",
+        "position": "estagiário",
+        "email": "ikaru.freitas@museumaccg.org.br",
+        "color_theme": "purple"
+    },
+    {
+        "id": 7,
+        "name": "Henrique Gomes da Silva",
+        "position": "Auxiliar de T.I",
+        "email": "Henrique.silva@museumaccg.org.br",
+        "color_theme": "red"
     }
 ]
